41c7a4091907252eb632e11db85c4681d85bf529
loveth10 <lovethokebe@gmail.com>
2023-02-02 18:15:50 -0800

    fixing

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-""" Recurse it!"""
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
 
 def recurse(subreddit, hot_list=[]):
-    """returns a list containing the titles of all hot articles
-    for a given subreddit"""
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': Alx-project by loveth'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
 
-    api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Alx-Project by loveth'}
-    params = {'limit': 1}
-    if len(hot_list) > 0:
-        after = list(hot_list[-1].keys())[0]
-        params['after'] = after
-
-    response = requests.get(api, headers=headers, allow_redirects=False,
-                            params=params)
-
-    if response.status_code == 200:
-        data_wrapper = response.json()
-        record = data_wrapper.get('data')
-        title = record.get('children')[0].get('data').get('title')
-        next = record.get('after')
-        if next:
-            hot_list.append({next: title})
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
             recurse(subreddit, hot_list)
-        else:
-            hot_list = [title for title in list(hot_list.values())]
-            return hot_list
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
     else:
-        return None
+        return (None)
